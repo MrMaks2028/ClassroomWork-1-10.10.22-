5194f153c3412431d2d6b227a7bea01c19f841aa
MAKS <MAKS@DESKTOP-P40RUSV>
2022-10-12 17:50:00 +0500

    Добавьте файлы проекта.

---
+++ ClassroomWork-1 (10.10.22)/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassroomWork_1__10._10._22_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int[] tmp = new int[args.Length];
+            int[] numbers = new int[args.Length];
+            int count = 0, sum = 0;
+
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                tmp[i] = Convert.ToInt32(args[i]);
+                if (tmp[i] % 2 == 0 && tmp[i] % 11 == 0)
+                {
+                    sum += tmp[i];
+                    count++;
+                }
+            }
+            numbers = new int[count];
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (tmp[i] % 2 == 0 && tmp[i] % 11 == 0)
+                {
+                    numbers[i] = tmp[i];
+                }
+            }
+            numbers.ToList();
+            Console.Write($"Сумма - {sum} и числа - ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
