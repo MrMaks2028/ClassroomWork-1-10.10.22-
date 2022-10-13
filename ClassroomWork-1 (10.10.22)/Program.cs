c1338adee117a9c53d02bd949ee310a4f31a80ae
MAKS <MAKS@DESKTOP-P40RUSV>
2022-10-13 18:18:16 +0500

    Добавил запись результатов в текстовые файлы

--- ClassroomWork-1 (10.10.22)/Program.cs
+++ ClassroomWork-1 (10.10.22)/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
 
 namespace ClassroomWork_1__10._10._22_
 {
@@ -11,7 +13,7 @@ namespace ClassroomWork_1__10._10._22_
         static void Main(string[] args)
         { 
             int[] tmp = new int[args.Length];
-            int[] numbers = new int[args.Length];
+            int[] nums = new int[args.Length];
             int count = 0, sum = 0;
 
             for (int i = 0; i < tmp.Length; i++)
@@ -23,20 +25,51 @@ namespace ClassroomWork_1__10._10._22_
                     count++;
                 }
             }
-            numbers = new int[count];
-            for(int i = 0; i < numbers.Length; i++)
+
+            nums = new int[count];
+
+            for (int i = 0; i < count; )
             {
-                if (tmp[i] % 2 == 0 && tmp[i] % 11 == 0)
+                for (int j = 0; j < tmp.Length; j++)
                 {
-                    numbers[i] = tmp[i];
+                    if (tmp[j] % 2 == 0 && tmp[j] % 11 == 0)
+                    {
+                        nums[i] = tmp[j];
+                        i++;
+                    }
                 }
             }
-            numbers.ToList();
-            Console.Write($"Сумма - {sum} и числа - ");
-            for (int i = 0; i < numbers.Length; i++)
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string summ = "summ.txt", numbers = "numbers.txt";
+
+            var sw = new StreamWriter(path + "\\" + summ, false);
+            sw.WriteLine($"Сумма - {sum}");
+            sw.Close();
+
+            sw = new StreamWriter(path + "\\" + numbers, false);
+            sw.Write("Числа - ");
+            for (int i = 0; i < nums.Length; i++)
             {
-                Console.WriteLine(numbers[i]);
+                sw.Write($"{nums[i]} ");
             }
+            sw.Close();
+
+            var sr = new StreamReader(path + "\\" + summ);
+            string line1;
+            while ((line1 = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line1);
+            }
+            sr.Close();
+
+            sr = new StreamReader(path + "\\" + numbers);
+            string line2;
+            while ((line2 = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line2);
+            }
+            sr.Close();
 
             Console.ReadKey();
         }
